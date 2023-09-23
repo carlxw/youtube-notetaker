0ffed904c996d560445904a04cddd225cb6c9d6e
carlxw <87335263+carlxw@users.noreply.github.com>
2023-09-23 13:59:22 -0400

    Update Popup.js

--- src/pages/Popup/Popup.js
+++ src/pages/Popup/Popup.js
@@ -3,79 +3,75 @@ import logo from '../../assets/img/logo.svg';
 import './Popup.css';
 
 const Popup = () => {
-  const [activeTab, setActiveTab] = useState(null);
+	const [activeTab, setActiveTab] = useState(null);
 
-  useEffect(() => {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-      // Since chrome.tabs.query is asynchronous, you should update the state
-      // with the result inside this callback.
-      if (tabs.length > 0) {
-        setActiveTab(tabs[0]);
-      }
-    });
-  }, []);
+	// Get the tab that the user is currently on
+	useEffect(() => {
+		chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+			if (tabs.length > 0) setActiveTab(tabs[0]);
+		});
+	}, []);
 
-  chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.action === 'test') {
-      console.log("Sent mute, returned here in test");
-      let time = sessionStorage.getItem("video_current_time");
-      console.log(`Current, the video only ran for ${ time } seconds`)
-    }
-    return true;
-  });
+	// Communication from the content script
+	chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+		if (message.action === 'test') {
+			console.log("Sent mute, returned here in test");
+		}
+		return true;
+	});
 
-  function resume(tab) {
-    sendMessage(tab, { action: "play" });
-  }
+	// Functionality to send messages to tabs
+	function sendMessage(tab, message) {
+		if (chrome.runtime.lastError) {
+			console.error(`Youtube Autopause error: ${chrome.runtime.lastError}`);
+			return;
+		}
 
-  function pause(tab) {
-    sendMessage(tab, { action: "pause" });
-  }
+		chrome.tabs.sendMessage(tab.id, message, {}, function (res) {
+			if (res) console.log(res)
+			void chrome.runtime.lastError;
+		});
+	}
 
-  function test(tab) {
-    sendMessage(tab, { action: "mute" })
-  }
+	function resume(tab) {
+		sendMessage(tab, { action: "play" });
+	}
 
-  // Functionality to send messages to tabs
-  function sendMessage(tab, message) {
-    if (chrome.runtime.lastError) {
-      console.error(`Youtube Autopause error: ${chrome.runtime.lastError}`);
-      return;
-    }
+	function pause(tab) {
+		sendMessage(tab, { action: "pause" });
+	}
 
-    chrome.tabs.sendMessage(tab.id, message, {}, function (res) {
-      if (res) console.log(res)
-      void chrome.runtime.lastError;
-    });
-  }
+	function test(tab) {
+		sendMessage(tab, { action: "mute" })
+	}
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!
-        </a>
-        <button onClick={() => {
-          resume(activeTab);
-        }}>Play the video</button>
-        <button onClick={() => {
-          pause(activeTab);
-        }}>Pause the video</button>
-        <button onClick={() => {
-          test(activeTab);
-        }}>Test</button>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/pages/Popup/Popup.jsx</code> and save to reload.
+				</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Learn React!
+				</a>
+				<button onClick={() => {
+					resume(activeTab);
+				}}>Play the video</button>
+				<button onClick={() => {
+					pause(activeTab);
+				}}>Pause the video</button>
+				<button onClick={() => {
+					test(activeTab);
+				}}>Test</button>
+			</header>
+		</div>
+	);
 };
 
 export default Popup;
