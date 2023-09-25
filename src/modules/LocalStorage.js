c956665ebd020d46153123aff08f4dba7cfe169b
carlxw <87335263+carlxw@users.noreply.github.com>
2023-09-25 16:04:39 -0400

    Update LocalStorage.js

--- src/modules/LocalStorage.js
+++ src/modules/LocalStorage.js
@@ -1,22 +1,78 @@
 /**
- * Prints all the keys associated with this app's local storage
+ * LEGEND
+ * ================
+ * ytmd_video_{url} - Markdown content for a video URL
+ * ytmd_content - Previously un-submitted content
  */
-function printKeys() {
-    for (let i = 0; i < localStorage.length; i++) {
-        console.log(localStorage.getItem(localStorage.key(i)));
+const textKey = "ytmd_content";
+const videoKey = "ytmd_video_";
+class LocalStorage {
+    /**
+     * Prints all the keys associated with this app's local storage
+     */
+    printKeys() {
+        for (let i = 0; i < localStorage.length; i++) {
+            console.log(localStorage.getItem(localStorage.key(i)));
+        }
+    }
+
+    // Markdown content ===========================================
+    /**
+     * Stores what is currently written to the local storage
+     *  
+     * @param {Object} obj The object to store to local data
+     */
+    setText(obj) {
+        localStorage.setItem(textKey, JSON.stringify(obj));
+    }
+
+    /**
+     * Gets what was written from the local storage
+     * 
+     * @returns Parsed object from local storage
+     */
+    getText() {
+        return JSON.parse(localStorage.getItem(textKey));
+    }
+
+    /**
+     * Clears the text of what was written
+     */
+    clearText() {
+        localStorage.removeItem(textKey);
+    }
+
+    // Notes content ==============================================
+    /**
+     * Deletes all video notes in the local storage
+     */
+    clearAllNotes() {
+        for (let i = 0; i < localStorage.length; i++) {
+            let key = localStorage.key(i);
+            if (key.includes(videoKey)) localStorage.removeItem(key);
+        }
+    }
+
+    /**
+     * Sets the serialized data to local storage
+     * 
+     * @param {Object} obj Serialized data that contains markdown object data
+     * @param {string} url The current URL the user is on, used to identify sessions
+     */
+    setNote(obj, url) {
+        localStorage.setItem(`${ videoKey }${ url }`, obj);
+    }
+
+    /**
+     * Restores the data from a previous session. This method will return null if
+     * there is no session to be restored from
+     * 
+     * @param {string} url The URL that the user is currently on
+     * @returns Object that contains the markdown data. 
+     */
+    getNote(url) {
+        return JSON.parse(localStorage.getItem(`${ videoKey }${ url }`));
     }
 }
 
-/**
- * Returns an item by key
- */
-function getByKey(key) {
-    return JSON.parse(localStorage.getItem(key));
-}
-
-/**
- * Sets an item by key
- */
-function setStorage(key, item) {
-    localStorage.setItem(key, JSON.stringify(item));
-}
+export default new LocalStorage();
